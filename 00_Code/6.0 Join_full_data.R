d0a9588fc4e6b995f8742df8d80828bd669d0817
José Daniel Conejeros <43749364+JDConejeros@users.noreply.github.com>
2026-04-13 06:07:07 -0400

    Adjust code structure

---
+++ 00_Code/6.0 Join_full_data.R
@@ -0,0 +1,10 @@
+# 6.0 Join data exposure and births -----
+rm(list = ls())
+
+## Settings ----
+source("00_Code/0.1 Settings.R")
+source("00_Code/0.2 Packages.R")
+source("00_Code/0.3 Functions.R")
+
+data_inp <- "01_Data/Output/"
+data_out <- "01_Data/Output/"
